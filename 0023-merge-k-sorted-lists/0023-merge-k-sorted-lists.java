70b83a367961646e429d060467fd30ef38835642
Soyeon Hong <52073383+shong91@users.noreply.github.com>
2023-02-01 00:36:58 +0900

    Time: 4 ms (82.10%), Space: 44.2 MB (66.37%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -18,6 +18,7 @@ class Solution {
     });
 
     for (ListNode list : lists) {
+        // 각 연결리스트의 루트를 pq 에 저장
         if(list != null) {
             pq.offer(list);
         }
@@ -28,14 +29,17 @@ class Solution {
 
     while (!pq.isEmpty()) {
       ListNode node = pq.poll();
-      if (node.next != null) {
-        pq.offer(node.next);
-      }
-      // swap
+
+      // swap - 다음 노드를 currentNode 로 저장
       currentNode.next = node;
       currentNode = currentNode.next;
+        
+     if (node.next != null) {
+        pq.offer(node.next);
+      }
     }
 
+    //Pop the smallest node and offer its next node if it is not null.
     return head.next;
     }
 }
