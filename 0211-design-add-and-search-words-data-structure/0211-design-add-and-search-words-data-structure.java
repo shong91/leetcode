80c43baf56c90a62729ac98c994d3e4fdb1e9870
Soyeon Hong <52073383+shong91@users.noreply.github.com>
2023-02-04 19:04:42 +0900

    Time: 857 ms (46.70%), Space: 85.1 MB (92.15%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,55 @@
+class WordDictionary {
+
+  private WordDictionary[] children;
+  private boolean isEnd;
+
+  public WordDictionary() {
+    children = new WordDictionary[26];
+    isEnd = false;
+  }
+
+  public boolean containsKey(char ch) {
+    return children[ch - 'a'] != null;
+  }
+
+  public WordDictionary get(char ch) {
+    return children[ch - 'a'];
+  }
+
+  public void put(WordDictionary word, char ch) {
+    children[ch - 'a'] = word;
+  }
+
+  public void addWord(String word) {
+    WordDictionary curr = this;
+    for (char c : word.toCharArray()) {
+      if (!curr.containsKey(c)) {
+        curr.put(new WordDictionary(), c);
+      }
+      curr = curr.get(c);
+    }
+    curr.isEnd = true;
+  }
+
+  public boolean search(String word) {
+    WordDictionary curr = this;
+    for (int i = 0; i < word.length(); i++) {
+      char c = word.charAt(i);
+      if (c == '.') {
+        for (WordDictionary ch : curr.children) {
+          if (ch != null && ch.search(word.substring(i+1))) {
+            return true;
+          }
+        }
+        return false;
+      }
+
+      if (!curr.containsKey(c)) {
+        return false;
+      }
+      curr = curr.get(c);
+    }
+
+    return curr != null && curr.isEnd;
+  }
+}
