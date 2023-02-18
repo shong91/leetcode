861ddebca50fc01c5a5889e6d052f3039498ad75
Soyeon Hong <52073383+shong91@users.noreply.github.com>
2023-02-18 18:17:52 +0900

    Time: 2 ms (95.33%), Space: 42.1 MB (94.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,30 +1,9 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-    
-    // check if both strings length are same
-    if (s.length() != s.length()) {
-      return false;
-    }
-
-    // 문제의 키 포인트! 제약 조건(문자열은 알파벳 소문자로 이루어짐) 을 이용하여 배열의 길이를 제한하고, 비트마스크 연산
-    // constraints: s and t consist of lowercase English letters.
-    int[] arr = new int[26];
-    for (int i = 0; i < s.length() ; i++) {
-      // 비트마스크를 이용
-      arr[s.charAt(i) - 'a']++;
-    }
-
-    for (int i = 0; i < t.length() ; i++) {
-      // 비트마스크를 이용
-      arr[t.charAt(i) - 'a']--;
-    }
-
-    for (int count : arr) {
-      if (count != 0 ) {
-        return false;
-      }
-    }
-
-    return true;
+       char[] sArr =s.toCharArray();
+        char[] tArr =t.toCharArray(); 
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        return new String(sArr).equals(new String(tArr));
     }
 }
