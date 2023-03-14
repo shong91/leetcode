62069e02b269acdb0c51a4514f45ca895d25f70b
Soyeon Hong <52073383+shong91@users.noreply.github.com>
2023-03-15 00:28:16 +0900

    Time: 2 ms (46.63%), Space: 40.9 MB (73.03%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -3,8 +3,19 @@ import java.math.BigInteger;
 
 class Solution {
     public String addBinary(String a, String b) {
-        BigInteger bigIntegerA = new BigInteger(a, 2);
-        BigInteger bigIntegerB = new BigInteger(b, 2);
-        return bigIntegerA.add(bigIntegerB).toString(2);
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+          if(i >= 0)
+            carry += a.charAt(i--) - '0';
+          if(j >= 0)
+            carry += b.charAt(j--) - '0';
+          sb.append(carry % 2);
+          carry /= 2;
+        }
+        return sb.reverse().toString();
     }
 }
