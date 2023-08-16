e9b4224958e5c9c40c13062973c363bb5315d5d6
Soyeon Hong <52073383+shong91@users.noreply.github.com>
2023-08-16 11:24:23 +0900

    Time: 42 ms (96.34%), Space: 22.4 MB (8.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,4 +1,9 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = [char.lower() for char in s if char.isalnum()]
-        return s == s[::-1]
+        new_string = [char.lower() for char in s if char.isalnum()]
+        
+        for i in range(len(new_string) // 2):
+            if new_string[i] != new_string[len(new_string) - 1 - i]:
+                return False
+
+        return True
