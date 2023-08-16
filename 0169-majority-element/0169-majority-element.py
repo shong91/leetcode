7980e9ad67d5a65fcbdf7fecfe3b116cf198c67b
Soyeon Hong <52073383+shong91@users.noreply.github.com>
2023-08-16 16:10:07 +0900

    Time: 140 ms (98.13%), Space: 17.9 MB (27.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,4 +2,13 @@ from collections import Counter
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return sorted(nums)[len(nums) // 2]
+        # solved O(1)
+        # 같은 값 -> count++, 다른 값-> count--
+        # if count == 0 -> re-set major
+        response, count = 0, 0
+        for num in nums:
+            # initially set
+            if count == 0:
+                response = num
+            count += 1 if num == response else -1
+        return response
