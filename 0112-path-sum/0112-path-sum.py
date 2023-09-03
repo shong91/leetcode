c8629a910c56579f47f9da6484251037123fc527
Soyeon Hong <52073383+shong91@users.noreply.github.com>
2023-09-03 18:36:14 +0900

    Time: 43 ms (87.76%), Space: 17.4 MB (77.46%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -11,6 +11,6 @@ class Solution:
         
         if not root.left and not root.right and targetSum == root.val:
             return True
-        
+        # answer 변수를 활용하여 answer += root.val 할 시 targetSum = 0 의 edge case 를 커버하지 못함 
         targetSum -= root.val
         return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
